61d1877eb3af2ad3e82621f1d038bf5c7874ae71
manvirsingh14 <manvirtaneja07@gmail.com>
2025-10-26 19:33:57 +0530

    added env log

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
+console.log("Backend URL:", BACKEND_URL);
+
 const axiosInstance = axios.create({
   baseURL: `${BACKEND_URL}/api/v1`,
   withCredentials: true,
