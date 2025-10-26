05f1e94f83fc3e7722fc6b8683d51a95539840f4
manvirsingh14 <manvirtaneja07@gmail.com>
2025-10-26 23:35:04 +0530

    fixed workout loading

--- src/features/workout/components/WorkoutArea.tsx
+++ src/features/workout/components/WorkoutArea.tsx
@@ -27,10 +27,6 @@ export const WorkoutArea = () => {
 
   const { isFetching, workouts, refetch } = useWorkouts(sessionId);
 
-  if (workouts.length === 0) {
-    return <EmptyArea date={date} setSessionId={setSessionId} />;
-  }
-
   const calculateHighestLoad = (sets: Set[]) => {
     const load = sets.map((set) => set.reps * set.weight);
     const highestLoad = Math.max(...load);
@@ -38,91 +34,95 @@ export const WorkoutArea = () => {
     return highestLoad;
   };
 
+  if (isFetching) {
+    return <PageLoader />;
+  }
+
+  if (workouts.length === 0 && !isFetching) {
+    return <EmptyArea date={date} setSessionId={setSessionId} />;
+  }
+
   return (
     <div className="h-full py-4">
-      {isFetching ? (
-        <PageLoader />
-      ) : (
-        <>
-          <ScrollArea className="w-full h-[90%]">
-            {workouts.map((workout) => {
-              let highestLoadSetId = "";
-              return (
-                <div
-                  onClick={() => {
-                    setExercise(workout.exerciseId);
-                    setOpenModal(true);
-                  }}
-                  key={workout._id}
-                  className="w-[90%] lg:w-[40%] mx-auto my-2 mb-4 cursor-pointer rounded shadow-sm py-2 border"
-                >
-                  <div className="font-medium text-lg text-center border-b-2 border-blue-500 pb-1 opacity-80">
-                    {workout.exerciseId.name}
-                  </div>
-                  <div className="mt-2 w-full">
-                    {workout.sets.map((set) => {
-                      if (highestLoadSetId.length < 1) {
-                        const isHighestLoadSet =
-                          set.reps * set.weight ===
-                          calculateHighestLoad(workout.sets);
-                        if (isHighestLoadSet) {
-                          highestLoadSetId = set._id;
-                        }
-                      }
-                      return (
-                        <div
-                          key={set._id}
-                          className={cn(
-                            "flex relative justify-between mt-3 mb-2 w-full px-6 lg:px-10 items-center"
-                          )}
-                        >
-                          <div className="min-w-6 flex">
-                            <IoIosTrophy
-                              className={cn(
-                                "text-blue-400",
-                                highestLoadSetId === set._id
-                                  ? "opacity-100"
-                                  : "opacity-0"
-                              )}
-                            />
-                          </div>
-                          <WorkoutAreaSetInformation
-                            information={set.weight}
-                            label="kgs"
-                          />
-                          <WorkoutAreaSetInformation
-                            information={set.reps}
-                            label="reps"
-                          />
-                          <div className="absolute right-[0%]">
-                            <WorkoutAreaSetMenu
-                              set={set}
-                              setEditOpenModal={setEditOpenModal}
-                              workoutId={workout._id}
-                              refetch={refetch}
-                            />
-                          </div>
-                        </div>
-                      );
-                    })}
-                  </div>
+      <>
+        <ScrollArea className="w-full h-[90%]">
+          {workouts.map((workout) => {
+            let highestLoadSetId = "";
+            return (
+              <div
+                onClick={() => {
+                  setExercise(workout.exerciseId);
+                  setOpenModal(true);
+                }}
+                key={workout._id}
+                className="w-[90%] lg:w-[40%] mx-auto my-2 mb-4 cursor-pointer rounded shadow-sm py-2 border"
+              >
+                <div className="font-medium text-lg text-center border-b-2 border-blue-500 pb-1 opacity-80">
+                  {workout.exerciseId.name}
                 </div>
-              );
-            })}
-          </ScrollArea>
-          <AddExerciseModal
-            exerciseDate={date}
-            openModal={openModal}
-            setOpenModal={setOpenModal}
-            refetch={refetch}
-          />
-          <EditExerciseModal
-            openEditModal={openEditModal}
-            setOpenEditModal={setEditOpenModal}
-            refetch={refetch}
-          />
-        </>
-      )}
+                <div className="mt-2 w-full">
+                  {workout.sets.map((set) => {
+                    if (highestLoadSetId.length < 1) {
+                      const isHighestLoadSet =
+                        set.reps * set.weight ===
+                        calculateHighestLoad(workout.sets);
+                      if (isHighestLoadSet) {
+                        highestLoadSetId = set._id;
+                      }
+                    }
+                    return (
+                      <div
+                        key={set._id}
+                        className={cn(
+                          "flex relative justify-between mt-3 mb-2 w-full px-6 lg:px-10 items-center"
+                        )}
+                      >
+                        <div className="min-w-6 flex">
+                          <IoIosTrophy
+                            className={cn(
+                              "text-blue-400",
+                              highestLoadSetId === set._id
+                                ? "opacity-100"
+                                : "opacity-0"
+                            )}
+                          />
+                        </div>
+                        <WorkoutAreaSetInformation
+                          information={set.weight}
+                          label="kgs"
+                        />
+                        <WorkoutAreaSetInformation
+                          information={set.reps}
+                          label="reps"
+                        />
+                        <div className="absolute right-[0%]">
+                          <WorkoutAreaSetMenu
+                            set={set}
+                            setEditOpenModal={setEditOpenModal}
+                            workoutId={workout._id}
+                            refetch={refetch}
+                          />
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </ScrollArea>
+        <AddExerciseModal
+          exerciseDate={date}
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+          refetch={refetch}
+        />
+        <EditExerciseModal
+          openEditModal={openEditModal}
+          setOpenEditModal={setEditOpenModal}
+          refetch={refetch}
+        />
+      </>
     </div>
   );
 };
