124a9f493f4a8eaeb6bf8ecbfee0ae5062db90d8
manvirsingh14 <manvirtaneja07@gmail.com>
2025-10-26 19:28:34 +0530

    fixed vite env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.BACKEND_URL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const axiosInstance = axios.create({
-  baseURL: `http://${BACKEND_URL}/api/v1`,
+  baseURL: `${BACKEND_URL}/api/v1`,
   withCredentials: true,
 });
 
